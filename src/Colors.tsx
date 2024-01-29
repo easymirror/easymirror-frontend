b214080027de264110b217a0f17a2df578a632ad
Matthew <45381190+Matthew17-21@users.noreply.github.com>
2024-01-29 09:55:59 -0500

    Create Colors.tsx

---
+++ src/Colors.tsx
@@ -0,0 +1,6 @@
+export const White = "#FFFFFF";
+export const JetBlack = "#000";
+export const Red = "#FF0000";
+export const MainPurple = "#9054FF";
+export const SharkBlue = "#3377FF";
+export const PythonGreen = "#58FF69";
