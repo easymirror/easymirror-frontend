cdf12b1f3e9a242829909ebc2d95f6fc2d8a46ca
Matthew <45381190+Matthew17-21@users.noreply.github.com>
2024-01-30 15:42:59 -0500

    Remove unused code

--- src/layouts/navbar/index.tsx
+++ src/layouts/navbar/index.tsx
@@ -2,28 +2,10 @@ import React from "react";
 import "./style.css";
 import style from "./navbar.module.scss"
 
-export const Navbar_ = () => {
-  return (
-    <div className="box">
-      <div className="navbar">
-        <div className="overlap-group">
-          <p className="easy-mirror">
-            <span className="text-wrapper">Easy</span>
-            <span className="span">Mirror</span>
-          </p>
-          <div className="div">Home</div>
-          <div className="text-wrapper-2">History</div>
-          <div className="text-wrapper-3">Account</div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
 export const Navbar = () => {
     return (
         <div className={style.navbar}>
-            {/* TODO EasyMirror Logo */}
+            {/* EasyMirror Logo */}
             <div className={style.logo}>
                 <p>
                     <span className={style.textWrapper}>Easy</span>
