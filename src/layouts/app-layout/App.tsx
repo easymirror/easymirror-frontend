1db66bcf35e0c6e5e2a69f217c7a23a7551f40de
Matthew <45381190+Matthew17-21@users.noreply.github.com>
2024-01-31 15:38:08 -0500

    Set height 100%

--- src/layouts/app-layout/App.tsx
+++ src/layouts/app-layout/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import './App.css';
+import style from "./app.module.scss"
 import { Navbar } from '../navbar';
 import { Route, Routes } from 'react-router';
 import { AccountPage } from '../../pages/Account';
 
 function App() {
   return (
-    <div className="app">
+    <div className={style.app}>
       <Navbar/>
       <div className="page-container">
           <Routes>
