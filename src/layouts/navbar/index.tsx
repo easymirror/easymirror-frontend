79f0d580b5adf5df922213211c9696a094ba55af
Matthew <45381190+Matthew17-21@users.noreply.github.com>
2024-01-31 11:57:39 -0500

    Added sidebar for mobile devices

--- src/layouts/navbar/index.tsx
+++ src/layouts/navbar/index.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import style from "./navbar.module.scss"
 import { SideNavigationButton } from "../../components/NavbarButton";
+import { useState } from "react";
 
 export const Navbar = () => {
+    const [isOpen, updateOpen] = useState(false)
+
     return (
         <nav className={style.navbar}>
+
+            {/* Response navBar for mobile clients */}
+            <span className={style.menuBtn} onClick={() =>updateOpen((prev) => !prev)}>&#9776;</span>
+            { isOpen && (
+                <div className={style.sideNav} id="sideNav">
+                    <div className={style.closeBtn} onClick={() =>updateOpen((prev) => !prev)}>&#10060;</div>
+                    <SideNavigationButton title="Home" to="/home"/>
+                    <SideNavigationButton title="History" to="/history"/>
+                    <SideNavigationButton title="Account" to="/account"/>
+                </div> 
+            )}
+
             {/* EasyMirror Logo */}
             <div className={style.logo}>
                 <p>
