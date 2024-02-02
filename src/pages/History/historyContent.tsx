891c51eaea50dd489b264ea3c89051e548ffedf9
Matthew <45381190+Matthew17-21@users.noreply.github.com>
2024-02-01 20:48:15 -0500

    Added styling to status box

--- src/pages/History/historyContent.tsx
+++ src/pages/History/historyContent.tsx
@@ -1,4 +1,5 @@
 import { History } from "./history"
+import style from "./styles.module.scss"
 interface HistoryContentProps {
     items: History[]
 }
@@ -14,7 +15,12 @@ export const HistoryContent = (props:HistoryContentProps) => {
                 <td><span>{item.link}</span></td>
                 <td><span>{item.date.toLocaleString()}</span></td>
                 <td><span>{item.duration}</span></td>
-                <td><span>{item.status}</span></td>
+                <td><div className={
+                  `${style.statusBox} ${item.status === "complete" ? style.complete : item.status === "failed" ? style.failed : style.inProgress}`
+                  }>
+                {item.status}
+                </div></td>
+
                 {/* TODO add info icon */}
                 {/* TODO add trash icon */}
             </tr>
