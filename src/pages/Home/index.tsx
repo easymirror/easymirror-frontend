06ad3b92dc97209278ae47517f3d17d2ac4c21c7
Matthew <45381190+Matthew17-21@users.noreply.github.com>
2024-02-02 10:52:16 -0500

    Update index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,8 +4,12 @@ import style from "./style.module.scss"
 export const HomePage = () => {
     return (
         <PageLayout title="EasyMirror" description="File sharing made easy!">
-        <div className={style.homePage}>
-        </div>
+            <div className={style.homePage}>
+                {/* TODO add drag & drop functionality */}
+                {/* TODO add btn to upload files */}
+                {/* TODO Add Slogan 1 */}
+                {/* TODO Add instructions */}
+            </div>
         </PageLayout>
     )
 }
