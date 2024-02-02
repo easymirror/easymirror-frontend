a29db084341d2b7e81a29edadf985b178a9a116c
Matthew <45381190+Matthew17-21@users.noreply.github.com>
2024-02-02 10:08:13 -0500

    Added copy to link

--- src/pages/History/historyContent.tsx
+++ src/pages/History/historyContent.tsx
@@ -2,6 +2,7 @@ import { HistoryItem } from "./historyItem"
 import style from "./styles.module.scss"
 import InfoIcon from '@mui/icons-material/Info';
 import DeleteIcon from '@mui/icons-material/Delete';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
 interface HistoryContentProps {
     items: HistoryItem[]
@@ -15,7 +16,14 @@ export const HistoryContent = (props:HistoryContentProps) => {
                 <td><span>{<input type="checkbox"/>}</span></td> 
                 <td><span>{item.id}</span></td>
                 <td><span>{item.nickname}</span></td>
-                <td><span>{item.link}</span></td>
+                <td>
+                  <div className={style.linkContainer}>
+                    <span>{item.link}</span>
+                    <div className={style.copy} onClick={() => {navigator.clipboard.writeText(item.link)}}>
+                      <ContentCopyIcon/>
+                    </div>
+                  </div>
+                </td>
                 <td><span>{item.date.toLocaleString()}</span></td>
                 <td><span>{item.duration}</span></td>
                 <td><div className={
