85d635254095167ed6b2b479e73aaef71c56c638
Matthew <45381190+Matthew17-21@users.noreply.github.com>
2024-03-11 15:28:13 -0400

    include missing dependency

--- src/hooks/useAxiosPrivate.tsx
+++ src/hooks/useAxiosPrivate.tsx
@@ -59,7 +59,7 @@ const useAxiosPrivate = () => {
             axiosPrivate.interceptors.request.eject(requestIntercept);
             axiosPrivate.interceptors.response.eject(responseIntercept);
         }
-    }, [auth, refresh])
+    }, [auth, refresh, initToken])
 
     return axiosPrivate;
 }
