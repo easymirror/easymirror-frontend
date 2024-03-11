b67589e97644f988148d810cc27aff94f0b11959
Matthew <45381190+Matthew17-21@users.noreply.github.com>
2024-03-11 15:11:50 -0400

    Copy host link functionality

--- src/pages/Mirrors/mirrorItem.tsx
+++ src/pages/Mirrors/mirrorItem.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react"
+import style from "./style.module.scss"
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
 interface props {
     index: number
@@ -10,15 +12,29 @@ interface props {
 export const MirrorItem = (props:props) => {
     const [date, setDate] = useState<Date>()
     useEffect(() => {
-        const d = new Date(props.upload_date);
-        setDate(d)
+        try {
+            console.log("props.upload_date = ",props.upload_date)
+            const d = new Date(props.upload_date);
+            setDate(d)
+        } catch (error) {
+            console.warn(error)
+        }
     }, [props.upload_date]);
 
     return (
         <tr key={props.index}>
             <td>{props.host}</td>
-            <td>{props.link ? props.link : "-"}</td>
-            <td>{props.link ? date.toLocaleString() : "-"}</td>
+            <td>
+                <div className={style.linkContainer}>
+                    <span>{props.link ? props.link : "-"}</span>
+                    {props.link && (
+                      <div className={style.copy} onClick={() => {navigator.clipboard.writeText(props.link)}}>
+                        <ContentCopyIcon/>
+                      </div>
+                    )}
+                </div>
+            </td>
+            <td>{props.link && date ? date.toLocaleString() : "-"}</td>
             <td>{props.link ? props.status : "-"}</td>
         </tr>
     )
