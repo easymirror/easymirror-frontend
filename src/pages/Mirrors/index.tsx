6d841c67f496afcbeea9d2802208d90bded37330
Matthew <45381190+Matthew17-21@users.noreply.github.com>
2024-03-11 14:36:11 -0400

    Update mirrors page

--- src/pages/Mirrors/index.tsx
+++ src/pages/Mirrors/index.tsx
@@ -1,44 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import { PageLayout } from "../../layouts/page-layout"
 import style from "./style.module.scss"
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import axios from "../../lib/axios";
+import axiosOg, {AxiosError} from "axios";
+import { NotFound } from "./notFound";
 
-const mirrorResponse = { "success": true, "id": "mirror_id_here", "name": "Some folder name here", "upload_date": "", "links": { "bunkr": "bunkr", "gofile": "", "pixeldrain": "", "cyberfile": "", "saint_to": "", "cyberdrop": "" } }
-const superlong = "some super long text that should hopefully overfill the cell contents and ellipsis"
+const MIRROR_PATH = "/api/v1/mirrors/"
+
+interface MirrorData {
+    success: boolean
+    error?: string
+    id?: string
+    name?: string
+    upload_date?: string
+    status?: string
+    links?: Links
+}
+  
+interface Links {
+    bunkr?: string
+    gofile?: string
+    pixeldrain?: string
+    cyberfile?: string
+    saint_to?: string
+    cyberdrop?: string
+}
+
 
 export const MirrorsPage = () => {
     const { id } = useParams();
-
-    // TODO: useEffect
-    // TODO: Get results from backend
-    // TODO: If no results found, display no results
+    const [mirrorData, updateMirrorData] = useState<MirrorData>({success: false})
+    const [error, setError] = useState<string | null>(null);
+    useEffect(() => {
+        // Get results from backend
+        const getMirrorData = async() => {
+            try {
+                const resp = await axios.get(MIRROR_PATH + id)
+                const response = resp.data as MirrorData
+                if (!response.success){
+                    // TODO: Handle this error better
+                    // TODO: If no results found, display no results
+                    console.warn("Failed because:", response.error)
+                }
+                updateMirrorData(response)
+            } catch (error) {
+                if (axiosOg.isAxiosError(error)) {
+                    const axiosError = error as AxiosError;
+                    if (axiosError.response?.status === 404) {
+                      setError('404 Not Found');
+                      console.log("404 not found")
+                    } else {
+                      setError(`HTTP Error: ${axiosError.response?.status}`);
+                    }
+                  } else {
+                    setError('An error occurred while fetching data');
+                  }
+            } 
+        }
+        getMirrorData()
+      }, [id]);
     return (
         <PageLayout title="Mirrors">
             <div className={style.accountPage}>
-                <div className={style.header}>{mirrorResponse.name !== "" ? mirrorResponse.name : `${mirrorResponse.id}'s Mirrors`}</div>
-                <div className={style.tableContainer}>
-                    <table className="mirrorTable">
-                    <tr>
-                        <th>Host</th>
-                        <th>Link</th>
-                        <th>Upload Date</th>
-                        <th>Status</th>
-                    </tr>
-                    {/* TODO: Dynamically render results */}
-                    <tr>
-                        <td>{superlong}</td>
-                        <td>{superlong}</td>
-                        <td>{superlong}</td>
-                        <td>{superlong}</td>
-                    </tr>
-                    <tr>
-                        <td>{superlong}</td>
-                        <td>{superlong}</td>
-                        <td>{superlong}</td>
-                        <td>{superlong}</td>
-                    </tr>
-                    </table>
-                </div>
+                {error && <NotFound/>}
+                {!error && (
+                    <>
+                    <div className={style.header}>{mirrorData.name ? mirrorData.name : `Mirror Links`}</div>
+                    <div className={style.tableContainer}>
+                        <table className="mirrorTable">
+                        <tr>
+                            <th>Host</th>
+                            <th>Link</th>
+                            <th>Upload Date</th>
+                            <th>Status</th>
+                        </tr>
+                        {mirrorData.links && Object.keys(mirrorData.links).map((key, index) => (
+                        <tr key={index}>
+                            <td>{key}</td>
+                            <td>{mirrorData.links[key]}</td>
+                            <td>{mirrorData.upload_date}</td>
+                            <td>{mirrorData.status}</td>
+                        </tr>
+                        ))}
+                        </table>
+                    </div>
+                    </>
+                )}
             </div>
         </PageLayout>
     )
