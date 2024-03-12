911964cc5a3078fe402c98d6c7cdb07b181a0bb7
Matthew <45381190+Matthew17-21@users.noreply.github.com>
2024-03-12 13:18:54 -0400

    Added env functionality

--- src/lib/axios.tsx
+++ src/lib/axios.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL: string = process.env.NODE_ENV === 'production' ? 
+process.env.REACT_APP_BASE_URL_PROD : process.env.REACT_APP_BASE_URL_DEV;
 
 export default axios.create({
     baseURL: BASE_URL
